67373229af58e5dd72432e45a074530e580a2f68
Rizky Tanjung <kimsohyuncans@gmail.com>
2019-08-22 21:33:54 +0700

    b

--- index.js
+++ index.js
@@ -12,35 +12,38 @@ app.use(bodyParser.json())
 
 //controllers
 const port = process.env.PORT || 8080;
-
-// router group
-app.group("/api/v1", (router) => {
-
-    router.get('/',Controllers.index)
-    
-    // register router
-    router.post('/regis',Controllers.register)
-
-    // login router 
-    router.post('/login', Controllers.login)
-
-    // update router
-    router.patch('/:id',Controllers.update)
-
-    // show all kokst
-    
-    router.get('/listkost',Controllers.showkost)
-
-    // add advertisement
-
-    // router.post('/add_addvertisement',Controllers.kost)
-
-    // // // add a booking kost
-    // router.post('/booking',Controllers.booking)
-
-    // wanna see my booking list
-    // router.post('/mybooking',Controllers.mybookinglist)
+app.get('/',(req,res)=> {
+    res.send('hello world');
 })
+
+// // router group
+// app.group("/api/v1", (router) => {
+
+//     router.get('/',Controllers.index)
+    
+//     // register router
+//     router.post('/regis',Controllers.register)
+
+//     // login router 
+//     router.post('/login', Controllers.login)
+
+//     // update router
+//     router.patch('/:id',Controllers.update)
+
+//     // show all kokst
+    
+//     router.get('/listkost',Controllers.showkost)
+
+//     // add advertisement
+
+//     // router.post('/add_addvertisement',Controllers.kost)
+
+//     // // // add a booking kost
+//     // router.post('/booking',Controllers.booking)
+
+//     // wanna see my booking list
+//     // router.post('/mybooking',Controllers.mybookinglist)
+// })
 
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
